81fda7e31e21735aae9c16e67ffd3985f5561160
TamNhien <canhthien91@gmail.com>
2024-06-17 15:59:35 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace CS_TinhTienVonLai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTien = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLai = new System.Windows.Forms.TextBox();
+            this.txt = new System.Windows.Forms.Label();
+            this.txtNam = new System.Windows.Forms.TextBox();
+            this.btnTinh = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.ltKQ = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số Tiền";
+            // 
+            // txtTien
+            // 
+            this.txtTien.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTien.Location = new System.Drawing.Point(134, 21);
+            this.txtTien.Name = "txtTien";
+            this.txtTien.Size = new System.Drawing.Size(125, 24);
+            this.txtTien.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 19);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Lãi Suất";
+            // 
+            // txtLai
+            // 
+            this.txtLai.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLai.Location = new System.Drawing.Point(134, 61);
+            this.txtLai.Name = "txtLai";
+            this.txtLai.Size = new System.Drawing.Size(125, 24);
+            this.txtLai.TabIndex = 1;
+            // 
+            // txt
+            // 
+            this.txt.AutoSize = true;
+            this.txt.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt.Location = new System.Drawing.Point(39, 106);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(65, 19);
+            this.txt.TabIndex = 0;
+            this.txt.Text = "Số Năm";
+            // 
+            // txtNam
+            // 
+            this.txtNam.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNam.Location = new System.Drawing.Point(134, 101);
+            this.txtNam.Name = "txtNam";
+            this.txtNam.Size = new System.Drawing.Size(125, 24);
+            this.txtNam.TabIndex = 1;
+            // 
+            // btnTinh
+            // 
+            this.btnTinh.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTinh.Location = new System.Drawing.Point(31, 358);
+            this.btnTinh.Name = "btnTinh";
+            this.btnTinh.Size = new System.Drawing.Size(87, 31);
+            this.btnTinh.TabIndex = 3;
+            this.btnTinh.Text = "Tính";
+            this.btnTinh.UseVisualStyleBackColor = true;
+            this.btnTinh.Click += new System.EventHandler(this.btnTinh_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(124, 358);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(87, 31);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(217, 358);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(87, 31);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // ltKQ
+            // 
+            this.ltKQ.FormattingEnabled = true;
+            this.ltKQ.Location = new System.Drawing.Point(44, 145);
+            this.ltKQ.Name = "ltKQ";
+            this.ltKQ.Size = new System.Drawing.Size(248, 186);
+            this.ltKQ.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 450);
+            this.Controls.Add(this.ltKQ);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnTinh);
+            this.Controls.Add(this.txtNam);
+            this.Controls.Add(this.txtLai);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.txtTien);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TÍNH TIỀN VỐN VÀ LÃI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTien;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLai;
+        private System.Windows.Forms.Label txt;
+        private System.Windows.Forms.TextBox txtNam;
+        private System.Windows.Forms.Button btnTinh;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ListBox ltKQ;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_TinhTienVonLai
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtTien.ResetText();
+            txtLai.ResetText();
+            txtNam.ResetText();
+            ltKQ.Items.Clear();
+        }
+
+        private void btnTinh_Click(object sender, EventArgs e)
+        {
+            ltKQ.Items.Clear();
+            double tienLai;
+            double laiSuat = double.Parse(txtLai.Text.Replace("%", "")) / 100;
+            double soTien = double.Parse(txtTien.Text);
+            int soNam = int.Parse(txtNam.Text);
+
+            for (int i = 1; i <= soNam; i++)
+            {
+                tienLai = soTien * laiSuat;
+                ltKQ.Items.Add($"{i}\t {soTien:F2}\t {tienLai:F2}");
+                soTien += tienLai;
+            }
+        }
+    }
+}
